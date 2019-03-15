c42715b02bf4b5e8a16c4bda295a82960770e147
K <kamiyaowl@gmail.com>
2019-03-15 23:23:31 +0900

    Distortionかいた

--- src/main/scala/audio/Distortion.scala
+++ src/main/scala/audio/Distortion.scala
@@ -9,13 +9,20 @@ class Distortion(width: Int) extends Module {
     val out = Output(SInt(width.W))
     // params
     val point = Input(SInt(width.W)) // 歪ませる変化点
-    val shift = Input(UInt(width.W)) // 傾きのレート
+    val shift = Input(UInt(width.W)) // 傾きのレート(右シフト数)
 
   })
+  val is_negative = RegInit(false.B)
   val abs_in = RegInit(0.S)
-  abs_in := Mux(io.in < 0.S, -io.in, io.in)
-  io.out := RegNext(abs_in)
+  is_negative := io.in < 0.S
+  abs_in := Mux(is_negative, -io.in, io.in)
 
+  when(abs_in > io.point) {
+    val diff = (abs_in - io.point) >> io.shift
+    io.out := RegNext(io.point + diff.asInstanceOf[SInt])
+  } .otherwise {
+    io.out := RegNext(io.in)
+  }
 }
 object Distortion extends App {
   chisel3.Driver.execute(args,()=>new Distortion(32 ))
