b106ea9c85cf34268b12980dd62e30f4fe8132b4
K <kamiyaowl@gmail.com>
2019-03-20 23:06:56 +0900

    FIFO Test Pass

--- src/main/scala/bf/Fifo.scala
+++ src/main/scala/bf/Fifo.scala
@@ -17,6 +17,10 @@ class Fifo(width: Int = 8, depthWidth: Int = 4)  extends Module {
     val outReady = Input(Bool())
     val outAck = Input(Bool())
     // status
+    val empty = Output(Bool())
+    val full = Output(Bool())
+    val inPtr = Output(UInt(depthWidth.W))
+    val outPtr = Output(UInt(depthWidth.W))
   })
   // io
   val inReady = RegInit(Bool(), false.B)
@@ -33,7 +37,8 @@ class Fifo(width: Int = 8, depthWidth: Int = 4)  extends Module {
   // ptrはどちらも***今いる場所のデータは無効***
   val inPtr = RegInit(UInt(depthWidth.W), 1.U) // 今いる場所にデータを書いたら進める
   val outPtr = RegInit(UInt(depthWidth.W), 0.U) // 読むときは一個先のデータを読んでPtrをその位置に動かす
-
+  io.inPtr := inPtr
+  io.outPtr := outPtr
   // status flags
   // 基本はoutPtr - inPtrで出るはず
   //[x outPtr(x) a b inPtr(x) x] : 4 - 1 (-1) => 2
@@ -47,14 +52,19 @@ class Fifo(width: Int = 8, depthWidth: Int = 4)  extends Module {
   val full = count === (depth.U - 3.U) // 余剰をもたせておく
   val inPtrNext = Mux(inPtr < depth.U - 1.U, inPtr + 1.U, 0.U)
   val outPtrNext = Mux(outPtr < depth.U - 1.U, outPtr + 1.U, 0.U)
-
+  io.empty := empty
+  io.full := full
   // in
   when(!full) {
     when(io.inValid) {
       mem.write(inPtr, io.inData)
       inPtr := inPtrNext // overflow対策
+      inReady := true.B
       inAck := true.B
+      printf(p"[Fifo] [euqueue] data:${io.inData} inPtr:$inPtr count:$count\n")
     } .otherwise {
+      printf(p"[Fifo] [euqueue] nack count:$count\n")
+      inReady := true.B
       inAck := false.B
     }
   } .otherwise {
@@ -63,21 +73,29 @@ class Fifo(width: Int = 8, depthWidth: Int = 4)  extends Module {
   }
 // out
   when(!empty) {
-    // Ackされてないときは保持
-    when(io.outAck) {
-      when(io.outReady) {
+    // readyになるまでは一切処置なし
+    when(io.outReady) {
+      // outReadyで受け側の準備はできている
+      // emptyではないので、Ackがあったかまだ未送信であれば
+      when(io.outAck | !outValid) {
+        printf(p"[Fifo] [dequeue] data:$outData outPtr:$outPtr count:$count\n")
         outData := mem.read(outPtrNext) // outPtr上は無効データ
         outPtr := outPtrNext
         outValid := true.B
       } .otherwise {
-        // 次に出せるデータがないので店じまい
-        outData := 0.U
-        outValid := false.B
+        // outAck = false && outValid = true
+        // つまり応答待ち
+        printf(p"[Fifo] [dequeue] keep count:$count\n")
       }
+    } .otherwise {
+      // 受けが準備できていないらしい
+      outData := 0.U
+      outValid := false.B
     }
   } .otherwise {
     // Ack以外は保持
     when(io.outAck) {
+      printf(p"[Fifo] [dequeue] negate(empty) count:$count\n")
       outData := 0.U
       outValid := false.B
     }
---
+++ src/test/scala/bf/FifoSpec.scala
@@ -0,0 +1,71 @@
+package bf
+
+import org.scalatest._
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+class FifoSpec extends ChiselFlatSpec {
+  "Fifo" should "Simple Behavior Test" in {
+    val din = 0x0 to 0xff
+    var dout = Seq[Int]()
+    val dequeueDelay = 4
+    val result = Driver(() => new Fifo(8, 4)) {
+      c =>  new PeekPokeTester(c) {
+        // init
+        poke(c.io.inData, 0.U)
+        poke(c.io.inValid, false.B)
+        poke(c.io.outReady, false.B)
+        poke(c.io.outAck, false.B)
+        step(5)
+        // enqueu and dequeue
+        poke(c.io.outReady, true.B)
+        poke(c.io.outReady, true.B)
+        poke(c.io.outAck, false.B)
+        step(1)
+        var loop = 0
+        for(d <- din) {
+          println(s"din:${din.dropWhile(_ != d).mkString}")
+          println(s"dout:${dout.mkString}")
+          if (peek(c.io.inReady) == BigInt(0)) {
+            println(s"\r[$loop] inReady == false")
+            step(1)
+          } else {
+            println(s"\r[$loop] [enqueue] data:$d")
+            poke(c.io.inData, d.U)
+            poke(c.io.inValid, true.B)
+          }
+          // dequeueはDelay遅らせた後にやる
+          if (loop > dequeueDelay) {
+            if (peek(c.io.outValid) == BigInt(1)) {
+              val data = peek(c.io.outData).toInt
+              dout = dout :+ data
+              println(s"\r[$loop] [dequeue] data:$data")
+              poke(c.io.outAck, true.B) // 次のデータにしておく
+            }
+          }
+          step(1)
+          // 後処理
+          poke(c.io.inData, d.U)
+          poke(c.io.inValid, false.B)
+          poke(c.io.outAck, false.B)
+          loop = loop + 1
+        }
+        // 残ったデータをすべて吐き出す
+        while(peek(c.io.outValid) == BigInt(1)) {
+          val data = peek(c.io.outData).toInt
+          dout = dout :+ data
+          println(s"\r[out of loop] [dequeue] data:$data")
+          poke(c.io.outAck, true.B) // 次のデータにしておく
+          step(1)
+        }
+        println(dout.mkString(","))
+      }
+    }
+    //
+    assert(result)
+    din.mkString.equals(dout.mkString)
+  }
+}
+
+
