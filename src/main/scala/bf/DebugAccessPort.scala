c697a5744ac629611440a0a409541338e92cbeb1
K <kamiyaowl@gmail.com>
2019-03-22 01:20:17 +0900

    buttonのチャタリング防止付きで配線

--- src/main/scala/bf/DebugAccessPort.scala
+++ src/main/scala/bf/DebugAccessPort.scala
@@ -9,7 +9,9 @@ class DebugAccessPort(
    // uart
    freq: Double = 100e6, baud: Double = 9600,
    // fifo
-   depthWidth: Int = 4
+   depthWidth: Int = 4,
+   // unti chattering
+   captureFreq: Double = 100, averageWidth: Int = 8, isPositive: Boolean = true
    ) extends Module {
   // for Arty A7
   val io = IO(new Bundle{
@@ -22,8 +24,9 @@ class DebugAccessPort(
     val uartRx = Input(Bool()) // A9(from FT2232 TXD)
 
     // !!! Arty A7 Hardware !!!
-    val buttons = Input(UInt(4.W)) // D9,C9,B9,B8(push buttons, active high)
-    val switches = Input(UInt(4.W)) // A8,C11,C10,A10
+    // val buttons = Input(UInt(4.W)) // D9,C9,B9,B8(push buttons, active high)
+    // 0-program, 1-run, 2-nc, 3-nc(topでreset)
+    val switches = Input(UInt(4.W)) // A8,C11,C10,A10,
 
     val leds = Output(UInt(4.W)) // H5,J5,T9,T10(green leds)
     val triLed0 = Output(UInt(3.W)) // G6,F6,E1(full color LED)
@@ -38,11 +41,17 @@ class DebugAccessPort(
   val fifoBfToDap = new Fifo(8, depthWidth)
 
   // physical system control
-  val program = RegInit(Bool(), false.B)
-  program := io.buttons(0)
-  val run1 = RegInit(Bool(), false.B)
-  val run2 = RegInit(Bool(), false.B)
-  val run3 = RegInit(Bool(), false.B)
+  // val chatterButton0 = new UntiChatter(freq, captureFreq, averageWidth, isPositive)
+  val chatterProgram = new UntiChatter(freq, captureFreq, averageWidth, isPositive)
+  val chatterRun = new UntiChatter(freq, captureFreq, averageWidth, isPositive)
+  val program = Wire(Bool())
+  val run = Wire(Bool())
+  io.switches(0) <> chatterProgram.io.din
+  io.switches(1) <> chatterRun.io.din
+  program <> chatterProgram.io.dout
+  run <> chatterProgram.io.dout
+  bf.io.run <> run
+  bf.io.program <> run
 
   // status indicator
   // 合成時にはset_false_path注釈をつけたほうがいいかも
